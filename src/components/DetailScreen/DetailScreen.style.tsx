9185c893706a0d03bef71e6f4a15c383e730ac06
Mauro Fernandez <mauro.e.n.fernandez@gmail.com>
2024-01-01 22:42:55 -0300

    Detail page css & buttons

--- src/components/DetailScreen/DetailScreen.style.tsx
+++ src/components/DetailScreen/DetailScreen.style.tsx
@@ -1,54 +1,87 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  fatherContainer: {
-    flex: 1,
-  },
   mainContainer: {
-    marginTop: `10%`,
-    paddingHorizontal: `5%`,
-    marginBottom: `10%`,
+    flex: 1,
+    paddingHorizontal: '5%',
+    paddingVertical: '5%',
+  },
+  scrollViewContainer: {
+    flexGrow: 1,
+    padding: 16,
+    backgroundColor: '#83d0c9',
+    borderWidth: 2,
+    borderColor: '#009688',
+    borderRadius: 20,
   },
   mainTitle: {
     fontSize: 24,
     fontWeight: '600',
-    textAlign: "center",
-    textAlignVertical: "center",
-    color: "#191919"
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: '#191919',
   },
   subTitle: {
     fontSize: 20,
     fontWeight: '500',
-    textAlign: "center",
-    textAlignVertical: "center",
-    color: "#191919",
-    marginBottom: 10
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: '#191919',
+    marginBottom: 10,
   },
   imageContainer: {
     flex: 1,
     borderWidth: 2,
-    borderColor: `#3700B3`,
+    borderColor: '#009688',
     borderRadius: 20,
-    height: 204,
   },
   image: {
     width: '100%',
-    height: 200, // Adjust the height as needed
-  },
-  container: {
-    flexGrow: 1,
-    padding: 16,
-    
+    height: 200,
     borderWidth: 2,
-    borderColor: `#3700B3`,
-    borderRadius: 20
+    borderColor: '#009688',
+    borderRadius: 20,
+  },
+  manyBoxContainer: {
+    flexDirection: "row",
+    marginVertical: 10,
+  },
+  separator: {
+    marginHorizontal: 5
+  },
+  boxContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#009688',
+    alignSelf: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    padding: 7,
+  },
+  boxTitle: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#191919',
+  },
+  boxSubtitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#191919',
+  },
+  extraMargin: {
+    marginTop: 10,
+    marginBottom: 40
   },
   buttonLeft: {
     position: 'absolute',
     bottom: 16,
-    left: 16,
-    padding: 10,
-    borderRadius: 8,
+    left: 40,
+    padding: 20,
+    borderRadius: 40,
     backgroundColor: '#3498db',
     alignItems: 'center',
     justifyContent: 'center',
@@ -56,9 +89,9 @@ export const styles = StyleSheet.create({
   buttonRight: {
     position: 'absolute',
     bottom: 16,
-    right: 16,
-    padding: 10,
-    borderRadius: 8,
+    right: 40,
+    padding: 20,
+    borderRadius: 40,
     backgroundColor: '#ff3377', // Adjust the color as needed
     alignItems: 'center',
     justifyContent: 'center',
@@ -66,5 +99,10 @@ export const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
+  },
+  laoderContainer: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
   },
 });
--- src/components/DetailScreen/DetailScreen.tsx
+++ src/components/DetailScreen/DetailScreen.tsx
@@ -3,6 +3,8 @@ import {View, Text, ScrollView, Image, TouchableOpacity} from 'react-native';
 import {styles} from './DetailScreen.style';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {getOneArtworkById} from '../../Api/artwork';
+import Icon from 'react-native-vector-icons/AntDesign'; // Import the icon library
+import LoaderComponent from '../Loader/Loader';
 
 interface Artwork {
   id: number;
@@ -28,7 +30,7 @@ const DetailScreenProps: React.FC = () => {
   const handleFetchArtworks = async () => {
     try {
       const apiData = await getOneArtworkById(artworkId);
-      console.log(apiData.data)
+      console.log(apiData.data);
 
       setArtworks(apiData.data);
       setArtworksImage(apiData.data.image_id);
@@ -48,43 +50,72 @@ const DetailScreenProps: React.FC = () => {
   };
 
   const handleAddToFavorites = () => {
-
     console.log('Added to favorites!');
   };
 
+  const descriptionFormatter = artworks?.description
+    ? artworks?.description.replace(/<[^>]*>/g, '')
+    : 'No description available';
+
   return (
-    <View style={styles.fatherContainer}>
-      <ScrollView 
-       // style={styles.mainContainer}
-        contentContainerStyle={styles.container}
-      >
-        <Text style={styles.mainTitle}>{artworks?.title}</Text>
-        <Text style={styles.subTitle}>{`by ${artworks?.artist_title}`}</Text>
-        <View style={styles.imageContainer}>
-          <Image
+    <>
+      {artworks && artworksImage ? (
+        <View style={styles.mainContainer}>
+          <ScrollView
+            contentContainerStyle={styles.scrollViewContainer}
+            showsVerticalScrollIndicator={false}>
+            <Text style={styles.mainTitle}>{artworks?.title}</Text>
+            <Text
+              style={styles.subTitle}>{`by ${artworks?.artist_title}`}</Text>
+
+            <Image
               source={{uri: imageUrl}}
               resizeMode="contain"
               style={styles.image}
             />
+            <View style={styles.manyBoxContainer}>
+              <View style={styles.boxContainer}>
+                <Text style={styles.boxTitle}>Year of display</Text>
+                <Text style={styles.boxSubtitle}>{artworks?.date_display}</Text>
+              </View>
+              <View style={styles.separator} />
+              <View style={styles.boxContainer}>
+                <Text style={styles.boxTitle}>Place of origin</Text>
+                <Text style={styles.boxSubtitle}>
+                  {artworks?.place_of_origin}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.boxContainer}>
+              <Text style={styles.boxTitle}>Dimensions</Text>
+              <Text style={styles.boxSubtitle}>
+                {`${artworks?.thumbnail.width} cm x ${artworks?.thumbnail.height} cm`}
+              </Text>
+            </View>
 
+            <View style={[styles.boxContainer, styles.extraMargin]}>
+              <Text style={styles.boxTitle}>Description</Text>
+
+              <Text style={styles.boxSubtitle}>{descriptionFormatter}</Text>
+            </View>
+          </ScrollView>
+
+          <TouchableOpacity style={styles.buttonLeft} onPress={handleGoBack}>
+            <Icon name="back" size={35} color="white" />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.buttonRight}
+            onPress={handleAddToFavorites}>
+            <Icon name="heart" size={35} color="white" />
+          </TouchableOpacity>
         </View>
-
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>{artworks?.date_display}</Text>
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>{artworks?.thumbnail.width}</Text>
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>{artworks?.thumbnail.height}</Text> 
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>{artworks?.place_of_origin}</Text>
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>{artworks?.description}</Text>
-
-      </ScrollView>
-
-      <TouchableOpacity style={styles.buttonLeft} onPress={handleGoBack}>
-          <Text style={styles.buttonText}>Go Back</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.buttonRight} onPress={handleAddToFavorites}>
-          <Text style={styles.buttonText}>Add to Favorites</Text>
-        </TouchableOpacity>
-      </View>
+      ) : (
+        <View style={styles.laoderContainer}>
+          <LoaderComponent />
+        </View>
+      )}
+    </>
   );
 };
 
